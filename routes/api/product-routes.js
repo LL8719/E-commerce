968644b9b8c813df09e4368792368f05e080f2a9
LL8719 <arsgatslr_87@yahoo.com>
2023-03-22 21:35:08 -0700

    delete route for product set

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -141,8 +141,25 @@ router.put('/:id', (req, res) => {
 		});
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
 	// delete one product by its `id` value
+	try {
+		const deletedProduct = await Product.destroy({
+			where: {
+				id: req.params.id,
+			},
+		});
+		if (deletedProduct) {
+			res.status(200).json(deletedProduct);
+		} else {
+			res.status(404).json({
+				message: 'Unable to delete product',
+			});
+		}
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ err: 'Internal server error' });
+	}
 });
 
 module.exports = router;
